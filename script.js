6971ab8b84989957571a9507d3591e9fcfcaba51
julioReis <juliokreis@hotmail.com>
2025-10-20 16:31:10 -0300

    Configurei icone do menu hamburguer novamente

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const openButton = document.getElementById('open-sidebar-button')
 const navbar = document.getElementById('navbar')
+
+const media = window.matchMedia('(max-width: 575px)')
+
+media.addEventListener('change', (e) => updateNavBar(e))
+
+function updateNavBar(e) {
+    const isMobile = e.matches
+    console.log(isMobile)
+    if (isMobile) {
+        navbar.setAttribute('inert', '')
+    }
+    else {
+        navbar.removeAttribute('inert')
+    }
+}
 
 function openSidebar() {
     navbar.classList.add('show')
+    openButton.setAttribute('aria-expanded', 'true')
 }
 
 function closeSidebar() {
     navbar.classList.remove('show')
+    openButton.setAttribute('aria-expanded', 'false')
+    navbar.removeAttribute('inert')
 }
+
+const navLinks = document.querySelectorAll('#nav a')
+navLinks.addEventListener('click', () => {
+    closeSidebar()
+})
+
+updateNavBar(media)
